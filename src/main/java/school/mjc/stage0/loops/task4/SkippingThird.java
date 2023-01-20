d9fc4e1fdf405d459e953546b44a6615ea05f18d
bakhridinova <bakhridinovaleyla@gmmail.com>
2023-01-20 13:23:56 +0500

    updated FixMe.java, NumberToBreakOn.java, SkippingThird.java, TwoRangesSum.java

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
-
+        int count = 1;
+        while (count < lastPrinted + 1) {
+            if (count % 3 != 0) {
+                System.out.println(count);
+            }
+            count++;
+        }
     }
 }
